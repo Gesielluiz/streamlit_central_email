384cb27f785f436bb136e52f9ac61c1d4384591d
GesielLuiz <102105522+Gesielluiz@users.noreply.github.com>
2025-05-21 14:17:58 -0300

    Update central_emails.py

--- central_emails.py
+++ central_emails.py
@@ -62,38 +62,41 @@ def pag_templates():
     st.markdown('# Templates')
     st.divider()
 
-    for template in listar_templates():
-        nome_template = template["nome_template"]
+    for i, arquivo in enumerate(PASTA_TEMPLATES.glob('*.txt')):
+        nome_arquivo = arquivo.stem.replace('_', ' ').upper()
+
         col1, col2, col3 = st.columns([0.6, 0.2, 0.2])
 
         col1.button(
-            nome_template.upper(),
-            key=f'{nome_template}',
+            nome_arquivo,
+            key=f'nome_template_{i}',
             use_container_width=True,
             on_click=_usar_template,
-            args=(nome_template,)
+            args=(nome_arquivo,)
         )
         col2.button(
             'EDITAR',
-            key=f'editar_{nome_template}',
+            key=f'editar_template_{i}',
             use_container_width=True,
             on_click=_editar_arquivo,
-            args=(nome_template,)
+            args=(nome_arquivo,)
         )
         col3.button(
             'REMOVER',
-            key=f'remover_{nome_template}',
+            key=f'remover_template_{i}',
             use_container_width=True,
             on_click=_remove_template,
-            args=(nome_template,)
+            args=(nome_arquivo,)
         )
 
     st.divider()
     st.button(
         'Adicionar Template',
+        key='botao_adicionar_template',
         on_click=mudar_pagina,
         args=('adicionar_novo_template',)
     )
+
 
 def pag_adicionar_novo_template(nome_template='', texto_template=''):
     nome_template = st.text_input('Nome do template', value=nome_template)
